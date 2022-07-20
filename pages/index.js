f974ec00c513dd2d3af921bff13f7fddb9d6b160
yantime <yanchota@gmail.com>
2022-07-19 19:55:34 -0500

    cambio clases

--- pages/index.js
+++ pages/index.js
@@ -29,11 +29,15 @@ export default function Home() {
       </Head>
 
 
-      <div className='container'>
+      <div classNameName='container'>
       
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="/">
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+
+            <Link href="/">
+              <a className="navbar-item"></a>
+            </Link>
+            
               
 
               <Image
@@ -46,30 +50,30 @@ export default function Home() {
       />
             </a>
         
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
             </a>
           </div>
         
-          <div id="navbarBasicExample" class="navbar-menu">
-            <div class="navbar-start">
-              <a class="navbar-item">
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item">
                 Inicio
               </a>
         
-              <a class="navbar-item">
+              <a className="navbar-item">
                 Nosotros
               </a>
         
               
             </div>
         
-            <div class="navbar-end">
-              <div class="navbar-item">
-                <div class="buttons">
-                  <a class="button is-success is-outlined" href="https://api.whatsapp.com/send?phone=51927580020&text=Hola%2C%20a%20cuanto%20esta%20la%20recarga%20de%20agua%3F" target="_blank" >
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <a className="button is-success is-outlined" href="https://api.whatsapp.com/send?phone=51927580020&text=Hola%2C%20a%20cuanto%20esta%20la%20recarga%20de%20agua%3F" target="_blank" >
                     Whatsapp: 927 580 020
                   </a>
                 </div>
@@ -78,13 +82,13 @@ export default function Home() {
           </div>
         </nav>
 
-      <div className="columns mt-2">
+      <div classNameName="columns mt-2">
 
-      <div className="column ">
+      <div classNameName="column ">
       
       <main>
 
-      <div className="content is-medium">
+      <div classNameName="content is-medium">
         
 
         <Image
@@ -100,37 +104,37 @@ export default function Home() {
       </div>
 
 
-      <section class="section has-background-info-light ">
+      <section className="section has-background-info-light ">
 
-      <div className="columns ">
+      <div classNameName="columns ">
       
-      <div className="column ">
+      <div classNameName="column ">
 
-        <h1 className="title is-1 mt-5" >
+        <h1 classNameName="title is-1 mt-5" >
            
           
-          <span class="has-text-danger"> MAKU</span>
+          <span className="has-text-danger"> MAKU</span>
           
           , el agua del Norte.
         </h1>
 
         
-        <div class="content">
+        <div className="content">
 
-        <p className="is-size-4">
+        <p classNameName="is-size-4">
         Agua Maku es ligera, baja en Sodio, debidamente tratada para cuidar tu salud.
         
           
         </p>
 
-        <p className="is-size-4">
+        <p classNameName="is-size-4">
           
-          Nuestro compromiso es brindarle a los consumidores Trujillanos un <strong> producto de calidad </strong> y un servicio abastecimiento eficiente y puntual.
+          Nuestro compromiso es brindarles a los consumidores Trujillanos un <strong> producto de calidad </strong> y un servicio abastecimiento eficiente y puntual.
           
             
           </p>
 
-          <p className="is-size-4">
+          <p classNameName="is-size-4">
           
             Contamos con los procesos más modernos de tratamiento de agua como es la <strong> ósmosis inversa y ozonización</strong>, para brindar a nuestros clientes un producto de calidad.
             
@@ -140,7 +144,7 @@ export default function Home() {
             
 
             <a href="https://api.whatsapp.com/send?phone=51927580020&text=Hola%2C%20a%20cuanto%20esta%20la%20recarga%20de%20agua%3F" target="_blank">
-              <button class="button is-medium is-success is-responsive"  >Escribir al Whatsapp</button>
+              <button className="button is-medium is-success is-responsive"  >Escribir al Whatsapp</button>
           </a>
 
             
@@ -150,7 +154,7 @@ export default function Home() {
       </div>
 
 
-      <div className="column ">
+      <div classNameName="column ">
 
 
         
@@ -173,12 +177,12 @@ export default function Home() {
     </section>
 
 
-    <section class="section">
+    <section className="section">
 
 
-      <div className="column  mb-2">
+      <div classNameName="column  mb-2">
 
-        <h2 className="title is-2 has-text-centered"> Todos los Productos</h2>
+        <h2 classNameName="title is-2 has-text-centered"> Todos los Productos</h2>
 
 
       </div>
@@ -186,14 +190,14 @@ export default function Home() {
 
 
 
-      <div className="columns ">
+      <div classNameName="columns ">
 
  
       
-        <div className="column">
+        <div classNameName="column">
 
         
-          <div class="box">
+          <div className="box">
           
           <Image
           src={galonera}
@@ -208,9 +212,9 @@ export default function Home() {
   
         </div>
   
-        <div className="column ">
+        <div classNameName="column ">
   
-          <div className="box">
+          <div classNameName="box">
           <Image
           src={bidon}
           alt="Agua de mesa maku en bidon, trujillo"
@@ -224,9 +228,9 @@ export default function Home() {
   
         </div>
 
-        <div className="column ">
+        <div classNameName="column ">
   
-          <div className="box">
+          <div classNameName="box">
           <Image
           src={recarga}
           alt="Recarga de Agua de mesa maku, trujillo"
@@ -246,13 +250,13 @@ export default function Home() {
 
       
       
-        <div className="columns ">
+        <div classNameName="columns ">
 
  
       
-          <div className="column ">
+          <div classNameName="column ">
     
-            <div className="box"> 
+            <div classNameName="box"> 
             <Image
             src={promo}
             alt="Agua de mesa maku, trujillo"
@@ -264,9 +268,9 @@ export default function Home() {
         </div>
           </div>
     
-          <div className="column ">
+          <div classNameName="column ">
     
-            <div className="box"> 
+            <div classNameName="box"> 
             <Image
             src={docena}
             alt="Agua de mesa maku, trujillo"
@@ -279,9 +283,9 @@ export default function Home() {
           </div>
         </div>
   
-          <div className="column ">
+          <div classNameName="column ">
     
-            <div className="box"> 
+            <div classNameName="box"> 
             <Image
             src={dispensador}
             alt="Agua de mesa maku, trujillo"
@@ -301,25 +305,29 @@ export default function Home() {
         </section>
 
 
-        <section class="section has-background-info-light">
+        <section className="section has-background-info-light">
 
-          <div className="columns is-mobile">
+          <div classNameName="columns is-mobile">
           
           
     
     
-          <div className="column is-full-mobile">
+          <div classNameName="column is-full-mobile">
 
-          <div className="content">
+          <div classNameName="content">
     
-          <h3 class="title is-2">Llámanos:</h3>
-          <a class="title is-2 has-text-info" href="tel:927580020">927 580 020</a>
+          <h3 className="title is-2">Llámanos:</h3>
+
+          <Link href="tel:927580020">
+            <a className="title is-2 has-text-info">927 580 020</a>
+          </Link>
+          
           <p></p>
-          <p class="is-size-5">Nuestras presentaciones de bidón de 20 litros y galonera de 7 litros</p>
-          <p class="is-size-5" >Delivery gratis a toda la ciudad de Trujillo, antendemos a casas, negocios y empresas.</p>
-          <p className="is-size-5"> Registro Sanitario: P0604322NLACRNR </p>
+          <p className="is-size-5">Nuestras presentaciones de bidón de 20 litros y galonera de 7 litros</p>
+          <p className="is-size-5" >Delivery gratis a toda la ciudad de Trujillo, atendemos a casas, negocios y empresas.</p>
+          <p classNameName="is-size-5"> Registro Sanitario: P0604322NLACRNR </p>
 
-          <p class="is-size-5"> <strong>Pedidos corporativos:</strong> nortprosac@gmail.com </p>
+          <p className="is-size-5"> <strong>Pedidos corporativos:</strong> nortprosac@gmail.com </p>
 
           </div>
     
@@ -347,12 +355,12 @@ export default function Home() {
 
       </div>
 
-      <footer className="footer">
+      <footer classNameName="footer">
 
-        <div class="columns">
-        <div  class="column is-desktop">
-        <div className="content has-text-centered">
-          <p class="is-size-5" >
+        <div className="columns">
+        <div  className="column is-desktop">
+        <div classNameName="content has-text-centered">
+          <p className="is-size-5" >
             <strong> Corporación Nortpro S.A.C - 2022 </strong>, todos los derechos reservados 
           </p>
         </div>
